a08888eb38d028e8917d9357eef924d0fa912135
27Chinedu <ce65@njit.edu>
2025-09-15 22:28:27 -0400

    Update __init__.py
    made changes to process division by 0 properly

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -44,5 +44,5 @@ def division(a: float, b: float) -> float:
     """
     if b == 0:
         # This part checks if 'b' is zero. If it is, we raise an error and stop the function.
-        raise ValueError("Division by zero is not allowed.")  # This sends an error message when someone tries to divide by zero.
-    return a / b  # If 'b' is not zero, we divide the first number (a) by the second number (b) and return the result.
+        if b == 0:
+            raise ZeroDivisionError("Division by zero is not allowed.")
